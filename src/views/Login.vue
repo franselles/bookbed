95380d9fcb84ec77919539f53b9ab59fa852dabe
franselles <fran.selles@gmail.com>
2020-06-19 12:43:53 +0200

    Fix navigation ios

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -43,7 +43,7 @@
       <div class="control">
         <b-taglist attached>
           <b-tag type="is-dark">version</b-tag>
-          <b-tag type="is-info">0.3.53</b-tag>
+          <b-tag type="is-info">0.3.54</b-tag>
         </b-taglist>
       </div>
     </form>
